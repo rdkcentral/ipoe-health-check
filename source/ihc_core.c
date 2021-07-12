f25df5759856ddf1b15b4979c2a5839bc1ee4604
Gautham Durairaj <gautham.durairaj@sky.uk>
2021-07-12 14:48:31 +0000

    RDKB-36183 : Compliance Issues in ipoe_health_check
    Reason for change: Compliance Issues in ipoe_health_check
    Test Procedure:
    	* Jenkins build should succeed
    Risks: Moderate
    Signed-off-by: gautham.durairaj@sky.uk
    
    Change-Id: I4b00c2711b9962261098947d00ee4011eef6c283

--- source/ihc_core.c
+++ source/ihc_core.c
@@ -1,24 +1,21 @@
 /*
- * Copyright (C) 2020 Sky
- * --------------------------------------------------------------------------
- * THIS SOFTWARE CONTRIBUTION IS PROVIDED ON BEHALF OF SKY PLC.
- * BY THE CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
- * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED
- * ******************************************************************
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2020 Sky
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
 /**************** System header files ***************************/
 #include <stdlib.h>
@@ -597,9 +594,8 @@ static uint16_t checksum(uint16_t *addr, int len)
         sum += *(uint8_t *)addr;
     }
 
-    // Fold 32-bit sum into 16 bits; we lose information by doing this,
-    // increasing the chances of a collision.
-    // sum = (lower 16 bits) + (upper 16 bits shifted right 16 bits)
+    // Implementation based on RFC1071.
+    // Fold 32-bit sum to 16 bits
     while (sum >> 16)
     {
         sum = (sum & CSUM_16_BIT_MASK) + (sum >> 16);
